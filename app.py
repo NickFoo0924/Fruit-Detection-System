74c034b9f04565fa3375283dc09af1f035a47f75
NickFoo0924 <chongxianfoo0924@gmail.com>
2025-09-07 17:21:43 +0800

    Fresh upload without large model files

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from tensorflow.keras.preprocessing.image import img_to_array
 # ======================
 # Load Models
 # ======================
-yolo_model_path = "best.pt"
-cnn_model_path = "cnn_fruit.h5"   # <- Add this file in your repo under /weights
+yolo_model_path = "https://drive.google.com/uc?id=1rpszJBxSTXYG_Z8xe_I_eXQD32RM4IIz"
+cnn_model_path = "https://drive.google.com/uc?id=1nxe-Jnx55t7lMX5MXg6avqrppu_mqawx"   # <- Add this file in your repo under /weights
 
 # Load YOLOv8 model
 yolo_model = YOLO(yolo_model_path)
